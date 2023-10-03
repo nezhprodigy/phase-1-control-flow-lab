6a1125be10c52db40f37ff24de6837ddac72627d
alex <alex.musyoki@student.moringaschool.com>
2023-10-03 16:11:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance <= 400) {
+    return 'This one is on me!'
+  }else if(distance > 2000 && distance < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }else if(distance > 2500) {
+    return 'No can do.'
+}
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+  if(city === 'NYC'){
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip) {
+    case 'generous' : return 'Thank you so much.'
+    break
+    case 'not as generous' : return 'Thank you.'
+    break
+    default : 
+    return 'Bye.'
+ }
 }
